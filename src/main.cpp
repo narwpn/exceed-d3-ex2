f7321b576553802540db83f03b10ab41ee8ea9ad
mkksdx2 <chaipot.k@ku.th>
2023-02-07 11:23:10 +0700

    First loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
+#define BLUE 5
+int delay_time = 0;
 
+void setup(){
+    Serial.begin(115200);
+    pinMode(BLUE, OUTPUT);
+
+    xTaskCreatePinnedToCore(loop, "loop", 1000, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(GET_delay, "GET_delay", 1000, NULL, 1, NULL, 1);
+}
 void loop() {
-  // put your main code here, to run repeatedly:
+    digitalWrite(BLUE, HIGH);
+    delay(delay_time);
+    digitalWrite(BLUE, LOW);
+    delay(delay_time);
 }
